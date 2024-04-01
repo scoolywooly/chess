c916ee5daa80262b8b64c7570e9063c7d2bb0d57
scoolywooly <bri22034@byui.edu>
2024-04-01 09:18:46 -0600

    summaraized some code so its mroe readable on the main

--- src/game.py
+++ src/game.py
@@ -159,36 +159,69 @@ def remove_piece(replace_with, piece=str, position=list, board=[
 
 
 
-def place_a_piece(): # Piece needs to be a string like "wb" or "bn" meaning white bisop, or black knight
+def place_a_piece(holding_a_piece): # Piece needs to be a string like "wb" or "bn" meaning white bisop, or black knight
+    
     click_location = get_mouse_pos()
+
     target_square = square_clicked(click_location)
+
     target_piece = piece_clicked(target_square, updated_chess_board)
 
     what_you_clicked = target_piece[TARGET_PIECE_INDEX]
+
+
     if what_you_clicked == "_": # If you clicked an empty square
         piece_to_be_removed = "_"
 
                 
     elif what_you_clicked in white_pieces_left: # If you are trying to take a piece
+
         piece_to_be_removed = target_piece[TARGET_PIECE_INDEX] # sets the piece to be captured
+
         white_pieces_left.remove(what_you_clicked) # takes the piece you captured out of the remaining list
 
     elif what_you_clicked in black_pieces_left:
+
         piece_to_be_removed = target_piece[TARGET_PIECE_INDEX] # sets the piece to be captured
+
         black_pieces_left.remove(what_you_clicked) # takes the piece you captured out of the remaining list
 
     else: # If you are just moving pieces
+
         piece_to_be_removed = target_piece[TARGET_PIECE_INDEX]
 
     updated_chess_board = remove_piece(piece_picked_up, target_piece[TARGET_PIECE_INDEX],target_piece[TARGET_POS_INDEX], updated_chess_board)
 
     if target_piece == "_":
-        taken_a_piece = False
+
+        holding_a_piece = False
                 
 
 
     piece_picked_up = piece_to_be_removed # clear the piece that was taken after being placed
 
+def take_a_piece(holding_a_piece):
+    # we find the square clicked, then remove the piece on that square
+    click_location = get_mouse_pos()
+
+    target_square = square_clicked(click_location)
+
+    target_piece = piece_clicked(target_square, updated_chess_board)
+
+
+
+    if target_piece[TARGET_PIECE_INDEX] == "_":
+                
+        piece_to_be_removed = target_piece[TARGET_PIECE_INDEX]
+    else:
+        piece_to_be_removed = "_"
+
+    updated_chess_board = remove_piece(piece_to_be_removed, target_piece[TARGET_PIECE_INDEX],target_piece[TARGET_POS_INDEX], updated_chess_board)
+
+    # prevent taking an empty square and placing an empty square somewhere else, jsut in case the player starts the game by clicking on the empty board.
+            
+    holding_a_piece = True
+    piece_picked_up = str(target_piece[TARGET_PIECE_INDEX])
 
 
 
